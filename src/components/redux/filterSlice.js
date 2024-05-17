c2e7d22167312a16d329c26196bfcf06e5accb97
Slav-0N <SOVILGO@gmail.com>
2024-05-17 21:35:43 +0300

    filterSlice

---
+++ src/components/redux/filterSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: "",
+  reducers: {
+    addFilter(state, action) {},
+  },
+});
